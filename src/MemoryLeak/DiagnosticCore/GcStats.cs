270677c4035d30fe8836b8c1f61f619dabd78aa4
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-02-26 20:13:33 +0900

    feat: add AllocatedMemory from GetTotalMemory

--- src/MemoryLeak/DiagnosticCore/GcStats.cs
+++ src/MemoryLeak/DiagnosticCore/GcStats.cs
@@ -15,14 +15,17 @@ namespace DiagnosticCore
         private static readonly Func<long> GetAllocatedBytesForCurrentThreadDelegate = CreateGetAllocatedBytesForCurrentThreadDelegate();
         private static readonly Func<bool, long> GetTotalAllocatedBytesDelegate = CreateGetTotalAllocatedBytesDelegate();
 
-        public static readonly GcStats Empty = new GcStats(0, 0, 0, 0, 0);
+        private static readonly Func<bool, long> GetTotalMemoryDelegate = CreateGetTotalMemoryDelegate();
 
-        private GcStats(int gen0Collections, int gen1Collections, int gen2Collections, long allocatedBytes, long totalOperations)
+        public static readonly GcStats Empty = new GcStats(0, 0, 0, 0, 0, 0);
+
+        private GcStats(int gen0Collections, int gen1Collections, int gen2Collections, long allocatedBytes, long totalAllocatedBytes, long totalOperations)
         {
             Gen0Collections = gen0Collections;
             Gen1Collections = gen1Collections;
             Gen2Collections = gen2Collections;
-            TotalAllocatedBytes = allocatedBytes;
+            AllocatedBytes = allocatedBytes;
+            TotalAllocatedBytes = totalAllocatedBytes;
             TotalOperations = totalOperations;
         }
 
@@ -32,9 +35,13 @@ namespace DiagnosticCore
         public int Gen2Collections { get; }
 
         /// <summary>
-        /// Total per all runs
+        /// Total per all runs (not affect from GC.Collect)
         /// </summary>
         private long TotalAllocatedBytes { get; }
+        /// <summary>
+        /// Actual (affect from GC.Collect)
+        /// </summary>
+        private long AllocatedBytes { get; }
 
         public long TotalOperations { get; }
 
@@ -59,6 +66,7 @@ namespace DiagnosticCore
                 left.Gen0Collections + right.Gen0Collections,
                 left.Gen1Collections + right.Gen1Collections,
                 left.Gen2Collections + right.Gen2Collections,
+                left.AllocatedBytes+ right.AllocatedBytes,
                 left.TotalAllocatedBytes + right.TotalAllocatedBytes,
                 left.TotalOperations + right.TotalOperations);
         }
@@ -69,12 +77,13 @@ namespace DiagnosticCore
                 Math.Max(0, left.Gen0Collections - right.Gen0Collections),
                 Math.Max(0, left.Gen1Collections - right.Gen1Collections),
                 Math.Max(0, left.Gen2Collections - right.Gen2Collections),
+                Math.Max(0, left.AllocatedBytes - right.AllocatedBytes),
                 Math.Max(0, left.TotalAllocatedBytes - right.TotalAllocatedBytes),
                 Math.Max(0, left.TotalOperations - right.TotalOperations));
         }
 
         public GcStats WithTotalOperations(long totalOperationsCount)
-            => this + new GcStats(0, 0, 0, 0, totalOperationsCount);
+            => this + new GcStats(0, 0, 0, 0, 0, totalOperationsCount);
 
         public int GetCollectionsCount(int generation)
         {
@@ -87,6 +96,20 @@ namespace DiagnosticCore
                 default:
                     return Gen2Collections;
             }
+        }
+
+        /// <summary>
+        /// returns current allocated bytes (per operation)
+        /// </summary>
+        /// <param name="excludeAllocationQuantumSideEffects">Allocation quantum can affecting some of our nano-benchmarks in non-deterministic way.
+        /// when this parameter is set to true and the number of all allocated bytes is less or equal AQ, we ignore AQ and put 0 to the results</param>
+        /// <returns></returns>
+        public long GetAllocatedBytes(bool excludeAllocationQuantumSideEffects)
+        {
+            if (!excludeAllocationQuantumSideEffects)
+                return AllocatedBytes;
+
+            return AllocatedBytes <= AllocationQuantum ? 0L : AllocatedBytes;
         }
 
         /// <summary>
@@ -108,10 +131,13 @@ namespace DiagnosticCore
             // this will force GC.Collect, so we want to do this before collecting collections counts
             long totalAllocatedBytes = GetTotalAllocatedBytesFromDelegate();
 
+            long allocatedBytes = GetAllocatedBytesFromDelegate();
+
             return new GcStats(
                 GC.CollectionCount(0),
                 GC.CollectionCount(1),
                 GC.CollectionCount(2),
+                allocatedBytes,
                 totalAllocatedBytes,
                 0);
         }
@@ -124,6 +150,7 @@ namespace DiagnosticCore
                 GC.CollectionCount(2),
 
                 // this will not GC.Collect, it may results inaccurate.
+                GetAllocatedBytesFromDelegate(false),
                 GetTotalAllocatedBytesFromDelegate(false),
                 0);
         }
@@ -137,12 +164,36 @@ namespace DiagnosticCore
 
                 // this will force GC.Collect, so we want to do this after collecting collections counts
                 // to exclude this single full forced collection from results
+                GetAllocatedBytesFromDelegate(),
                 GetTotalAllocatedBytesFromDelegate(),
                 0);
         }
 
         public static GcStats FromForced(int forcedFullGarbageCollections)
-            => new GcStats(forcedFullGarbageCollections, forcedFullGarbageCollections, forcedFullGarbageCollections, 0, 0);
+            => new GcStats(forcedFullGarbageCollections, forcedFullGarbageCollections, forcedFullGarbageCollections, 0, 0, 0);
+
+        private static long GetAllocatedBytesFromDelegate(bool forceGc = true)
+        {
+            if (Portability.RuntimeInformation.IsMono) // Monitoring is not available in Mono, see http://stackoverflow.com/questions/40234948/how-to-get-the-number-of-allocated-bytes-
+                return 0;
+
+            if (forceGc)
+            {
+                // "This instance Int64 property returns the number of bytes that have been allocated by a specific
+                // AppDomain. The number is accurate as of the last garbage collection." - CLR via C#
+                // so we enforce GC.Collect here just to make sure we get accurate results
+                GC.Collect();
+            }
+
+            if (Portability.RuntimeInformation.IsFullFramework) // it can be a .NET app consuming our .NET Standard package
+                return AppDomain.CurrentDomain.MonitoringTotalAllocatedMemorySize;
+
+            if (GetTotalMemoryDelegate != null)
+                return GetTotalMemoryDelegate.Invoke(forceGc); // true for the "precise" argument
+
+            // https://apisof.net/catalog/System.GC.GetAllocatedBytesForCurrentThread() is not part of the .NET Standard, so we use reflection to call it..
+            return GetAllocatedBytesForCurrentThreadDelegate.Invoke();
+        }
 
         private static long GetTotalAllocatedBytesFromDelegate(bool forceGc = true)
         {
@@ -185,6 +236,15 @@ namespace DiagnosticCore
             return method != null ? (Func<bool, long>)method.CreateDelegate(typeof(Func<bool, long>)) : null;
         }
 
+        private static Func<bool, long> CreateGetTotalMemoryDelegate()
+        {
+            // this method is not a part of .NET Standard so we need to use reflection
+            var method = typeof(GC).GetTypeInfo().GetMethod("GetTotalMemory", BindingFlags.Public | BindingFlags.Static);
+
+            // we create delegate to avoid boxing, IMPORTANT!
+            return method != null ? (Func<bool, long>)method.CreateDelegate(typeof(Func<bool, long>)) : null;
+        }
+
         public string ToOutputLine()
             => $"{ResultsLinePrefix} {Gen0Collections} {Gen1Collections} {Gen2Collections} {TotalAllocatedBytes} {TotalOperations}";
 
@@ -197,13 +257,14 @@ namespace DiagnosticCore
             if (!int.TryParse(measurementSplit[0], out int gen0)
                 || !int.TryParse(measurementSplit[1], out int gen1)
                 || !int.TryParse(measurementSplit[2], out int gen2)
-                || !long.TryParse(measurementSplit[3], out long allocatedBytes)
-                || !long.TryParse(measurementSplit[4], out long totalOperationsCount))
+                || !int.TryParse(measurementSplit[3], out int allocatedBytes)
+                || !long.TryParse(measurementSplit[4], out long totalAllocatedBytes)
+                || !long.TryParse(measurementSplit[5], out long totalOperationsCount))
             {
                 throw new NotSupportedException("Invalid string");
             }
 
-            return new GcStats(gen0, gen1, gen2, allocatedBytes, totalOperationsCount);
+            return new GcStats(gen0, gen1, gen2, allocatedBytes, totalAllocatedBytes, totalOperationsCount);
         }
 
         public override string ToString() => ToOutputLine();
@@ -237,7 +298,7 @@ namespace DiagnosticCore
             return result;
         }
 
-        public bool Equals(GcStats other) => Gen0Collections == other.Gen0Collections && Gen1Collections == other.Gen1Collections && Gen2Collections == other.Gen2Collections && TotalAllocatedBytes == other.TotalAllocatedBytes && TotalOperations == other.TotalOperations;
+        public bool Equals(GcStats other) => Gen0Collections == other.Gen0Collections && Gen1Collections == other.Gen1Collections && Gen2Collections == other.Gen2Collections && AllocatedBytes == other.AllocatedBytes && TotalAllocatedBytes == other.TotalAllocatedBytes && TotalOperations == other.TotalOperations;
 
         public override bool Equals(object obj) => obj is GcStats other && Equals(other);
 
@@ -248,6 +309,7 @@ namespace DiagnosticCore
                 int hashCode = Gen0Collections;
                 hashCode = (hashCode * 397) ^ Gen1Collections;
                 hashCode = (hashCode * 397) ^ Gen2Collections;
+                hashCode = (hashCode * 397) ^ AllocatedBytes.GetHashCode();
                 hashCode = (hashCode * 397) ^ TotalAllocatedBytes.GetHashCode();
                 hashCode = (hashCode * 397) ^ TotalOperations.GetHashCode();
                 return hashCode;
